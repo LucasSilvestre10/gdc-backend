025465c2c4040198b2446eb2094b1e7ecbe8ee60
Lucas Silvestre <lucas.silvestre.dev@hotmail.com>
2025-09-06 15:58:00 -0300

    feat: add _id property to DocumentType model

--- src/models/DocumentType.ts
+++ src/models/DocumentType.ts
@@ -3,6 +3,9 @@ import { Model, ObjectID } from "@tsed/mongoose";
 
 @Model()
 export class DocumentType {
+  @Property(ObjectID)
+  _id?: ObjectID; // Usar _id padrão do Mongoose
+
   @Required()
   @Property(String)
   name!: string;
