34f63f9840990f5b16b69836da07cf33dc2f7935
Lucas Silvestre <lucas.silvestre.dev@hotmail.com>
2025-09-07 01:25:35 -0300

    feat: add coverage configuration for Vitest and include coverage-v8 dependency

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: ["./test/setup.ts"]
+    setupFiles: ["./test/setup.ts"],
+    coverage: {
+      include: ["src"], // verifica cobertura apenas dos arquivos em src/
+      exclude: ["test", "node_modules", "config", "src/config", "src/mongo-connection.ts", "src/index.ts", "src/**/*.spec.ts", "src/Server.ts"], // ignora testes, configs e node_modules
+      reporter: ["text", "html"]
+    }
   }
 });
