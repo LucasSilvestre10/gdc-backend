52f5e5d20e599e31066d8cf13520c36cade7af3a
Lucas Silvestre <lucas.silvestre.dev@hotmail.com>
2025-09-13 00:53:11 -0300

    refactor: clean up orphaned configuration files

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -3,17 +3,3 @@ export const mongooseConfig = {
   url: "mongodb://admin:secret@localhost:27017/appdb?authSource=admin",
   connectionOptions: {},
 };
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-
-export async function connectInMemoryMongo() {
-  const mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  await mongoose.connect(uri);
-  return mongoServer;
-}
-
-export async function disconnectInMemoryMongo(mongoServer: MongoMemoryServer) {
-  await mongoose.disconnect();
-  await mongoServer.stop();
-}
